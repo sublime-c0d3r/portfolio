8c394333de47ea394a9f657160357b2ad0b063c5
Amandeep Singh <iamSokhi@gmail.com>
2021-09-09 01:29:40 +0530

    Implementing forms for netlify

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // ---
 const hamMenuBtn = document.querySelector(".header__main-ham-menu-cont");
 const smallMenu = document.querySelector(".header__sm-menu");
-const submitButton = document.querySelector("#submit");
 
 hamMenuBtn.addEventListener("click", () => {
   if (smallMenu.classList.contains("header__sm-menu--active")) {
@@ -16,26 +15,4 @@ const headerLogoConatiner = document.querySelector(".header__logo-container");
 
 headerLogoConatiner.addEventListener("click", () => {
   location.href = "index.html";
-});
-
-submitButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  const contactForm = document.querySelector(".contact__form");
-  const formData = new FormData(contactForm);
-  const getFormValue = (item) => formData.get(item);
-  if (
-    !getFormValue("name") ||
-    !getFormValue("email") ||
-    !getFormValue("message")
-  ) {
-    window.alert("Please fillin mandatory fields");
-    return;
-  }
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString(),
-  })
-    .then(() => console.log("Form submitted successfully."))
-    .catch((e) => console.log(e));
 });
