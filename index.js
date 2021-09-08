26c4ec7f1075ed3539350ce1f94fcb81d8b41c38
Amandeep Singh <iamSokhi@gmail.com>
2021-09-09 00:57:36 +0530

    implementing netlify forms

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // ---
 const hamMenuBtn = document.querySelector(".header__main-ham-menu-cont");
 const smallMenu = document.querySelector(".header__sm-menu");
-const submitButton = document.querySelector("#submit");
 
 hamMenuBtn.addEventListener("click", () => {
   if (smallMenu.classList.contains("header__sm-menu--active")) {
@@ -16,32 +15,4 @@ const headerLogoConatiner = document.querySelector(".header__logo-container");
 
 headerLogoConatiner.addEventListener("click", () => {
   location.href = "index.html";
-});
-
-submitButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  const name = document.querySelector("#name").value;
-  const email = document.querySelector("#email").value;
-  const message = document.querySelector("#message").value;
-  if (!name || !email || !message) {
-    window.alert("Please fillin mandatory fields");
-    return;
-  }
-  const formData = new FormData();
-  formData.set("name", name);
-  formData.set("email", email);
-  formData.set("message", message);
-  fetch("https://formsubmit.co/ajax/developeramansingh1@gmail.com", {
-    method: "POST",
-    data: formData,
-    dataType: "json",
-  })
-    .then((resp) => resp.json())
-    .then((response) => {
-      if (response.success === "true") {
-        document.querySelector(".contact > .main-container").style.display =
-          "none";
-        document.querySelector(".thankyou-container").style.display = "block";
-      }
-    });
 });
