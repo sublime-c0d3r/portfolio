6059072bb0046c64dcc75f8f6eebddb46ef9daad
Amandeep Singh <iamSokhi@gmail.com>
2021-09-09 01:17:03 +0530

    Implementing form ajax for netlify

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // ---
 const hamMenuBtn = document.querySelector(".header__main-ham-menu-cont");
 const smallMenu = document.querySelector(".header__sm-menu");
+const submitButton = document.querySelector("#submit");
 
 hamMenuBtn.addEventListener("click", () => {
   if (smallMenu.classList.contains("header__sm-menu--active")) {
@@ -15,4 +16,26 @@ const headerLogoConatiner = document.querySelector(".header__logo-container");
 
 headerLogoConatiner.addEventListener("click", () => {
   location.href = "index.html";
+});
+
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const contactForm = document.querySelector(".contact__form");
+  const formData = new FormData(contactForm);
+  const getFormValue = (item) => formData.get(item);
+  if (
+    !getFormValue("name") ||
+    !getFormValue("email") ||
+    !getFormValue("message")
+  ) {
+    window.alert("Please fillin mandatory fields");
+    return;
+  }
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => console.log("Form submitted successfully."))
+    .catch((e) => console.log(e));
 });
